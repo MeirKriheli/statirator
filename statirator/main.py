296c82c87017d2e7efc52ac2006cf9728eb5167f
Meir Kriheli <mkriheli@gmail.com>
2011-09-30 11:24:12 +0300

    Change options

--- statirator/main.py
+++ statirator/main.py
@@ -1,12 +1,12 @@
 from tornado import options
 import logging
 
-VALID_ARGS = ('init', 'genrate', 'serve')
+VALID_ARGS = ('init', 'build', 'serve')
 
 def main():
     args = options.parse_command_line()
 
-    if args not in VALID_ARGS:
+    if not args or args[0] not in VALID_ARGS:
         valid_opts = ', '.join(VALID_ARGS)
         logging.error('Invalid option. Valid options are {0}'.format(valid_opts))
         options.print_help()
