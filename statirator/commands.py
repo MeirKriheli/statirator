925ff4987902dafb9c61c3394598741e7b4ce6d1
Meir Kriheli <mkriheli@gmail.com>
2011-09-30 13:59:56 +0300

    Validated the initial dir doesn't exist

--- statirator/commands.py
+++ statirator/commands.py
@@ -1,9 +1,21 @@
 """Handles cli arguments"""
 import logging
 import sys
+import os
 
 def init(args, options):
     """Create the initial site"""
+
+    if len(args) != 2:
+        logging.error('init takes a single argument: dir name')
+        options.print_help()
+        sys.exit(1)
+
+    root_dir = args[1]
+
+    if os.path.exists(root_dir):
+        logging.error('{0} already exists, aborting'.format(root_dir))
+        sys.exit(1)
 
     logging.error('Not implemented')
     sys.exit(1)
