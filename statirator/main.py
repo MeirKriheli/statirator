e7f69853b0580406c0e82aaafba740bd193f9c8d
Meir Kriheli <mkriheli@gmail.com>
2012-02-26 02:11:04 +0200

    Setup logging

--- statirator/main.py
+++ statirator/main.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 from . import commands
 
 def create_options():
@@ -7,22 +8,27 @@ def create_options():
     parser = argparse.ArgumentParser(
             'Staitrator - Static multilingual site and blog generator')
 
+    log_level_choices = ['error', 'info', 'debug', 'none']
+    parser.add_argument('--logging', choices=log_level_choices,
+        default='info',
+        help='Log level [Default: %(default)s]. "none" won\'t touch logging')
+
     sub_parsers = parser.add_subparsers(help='Sub Commands help')
     init = sub_parsers.add_parser('init', help='Initiate a new site')
 
     init.add_argument('directory', help='Target directory')
     init.add_argument('-n', '--name', default='Default site',
-            help='Site name and title [default: %(default)s]')
+            help='Site name and title [Default: %(default)s]')
     init.add_argument('-c', '--site_class', default='statirator.site.Html5Site',
-            help='The base class for the site [default: %(default)s]')
+            help='The base class for the site [Default: %(default)s]')
     init.add_argument('-s', '--source', default='source',
-            help="Site's source directory [default: %(default)s]")
+            help="Site's source directory [Default: %(default)s]")
     init.add_argument('-b', '--build', default='build',
-            help="Site's build directory [default: %(default)s]")
+            help="Site's build directory [Default: %(default)s]")
 
     init.add_argument('-l', '--languages', nargs='*', dest='languages',
             default=['he', 'en'], help='Supported languages.'
-                ' [default: %(default)s]')
+                ' [Default: %(default)s]')
 
     init.set_defaults(func=commands.init)
 
@@ -35,10 +41,18 @@ def create_options():
 
     return parser
 
+def setup_logging(args):
+    if args.logging == 'none':
+        return
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(getattr(logging, args.logging.upper()))
+
 def main():
     parser = create_options()
     args = parser.parse_args()
 
+    setup_logging(args)
     args.func(args)
 
 if __name__ == '__main__':
