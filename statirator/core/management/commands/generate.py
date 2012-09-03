c53fde41a33433fa5fe2cf76e7049001236ef4cb
Meir Kriheli <mkriheli@gmail.com>
2012-09-04 02:00:04 +0300

    Rename walkers to readers

--- statirator/core/management/commands/generate.py
+++ statirator/core/management/commands/generate.py
@@ -1,7 +1,7 @@
 import logging
 
 from django.core.management.base import BaseCommand
-from statirator.utils import find_walkers
+from statirator.utils import find_readers
 
 
 class Command(BaseCommand):
@@ -9,11 +9,12 @@ class Command(BaseCommand):
     help = "Walk the resources, builds the db, and generates the static site"
 
     def handle(self, *args, **options):
-        self.walkers = find_walkers()
+        self.readers = find_readers()
+        self.read_resources()
 
-    def walk(self):
-        """Walk to walkers to populate the db"""
+    def read_resources(self):
+        """Walk to readers to populate the db"""
 
-        for walker in self.walkers:
-            logging.info('Walking %s', walker)
-            walker()
+        for reader in self.readers:
+            logging.info('Reading with %s', reader)
+            reader()
--- statirator/utils.py
+++ statirator/utils.py
@@ -1,33 +1,33 @@
 import logging
 
 
-def find_walkers():
-    """Auto discover walkers in installed apps.
+def find_readers():
+    """Auto discover readers in installed apps.
 
-    Each walkers.py should have the WALKERS list with walker classes
+    Each readers.py should have the READERS list with reader classes
     """
 
     from django.conf import settings
     from django.utils.importlib import import_module
     from django.utils.module_loading import module_has_submodule
 
-    walkers = []
+    readers = []
     for app in settings.INSTALLED_APPS:
         mod = import_module(app)
         # Attempt to import the app's admin module.
         try:
-            walk_mod = import_module('%s.walkers' % app)
-            if hasattr(walk_mod, 'WALKERS'):
-                walkers.extend(walk_mod.WALKERS)
+            walk_mod = import_module('%s.readers' % app)
+            if hasattr(walk_mod, 'READERS'):
+                readers.extend(walk_mod.READERS)
             else:
                 logging.warn(
-                    'walkers found in app %s, but has no WALKERS attrib',
+                    'readers found in app %s, but has no READERS attrib',
                     app)
         except:
             # Decide whether to bubble up this error. If the app just
-            # doesn't have an walkers module, we can ignore the error
+            # doesn't have an readers module, we can ignore the error
             # attempting to import it, otherwise we want it to bubble up.
-            if module_has_submodule(mod, 'walkers'):
+            if module_has_submodule(mod, 'readers'):
                 raise
 
-    return walkers
+    return readers
