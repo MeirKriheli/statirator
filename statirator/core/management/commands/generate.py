8b10dfaa38f906a5552c98376eac4dfa5cc16948
Meir Kriheli <mkriheli@gmail.com>
2012-09-04 01:55:42 +0300

    Walk the walkers to populate the db

--- statirator/core/management/commands/generate.py
+++ statirator/core/management/commands/generate.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.core.management.base import BaseCommand
 from statirator.utils import find_walkers
 
@@ -7,6 +9,11 @@ class Command(BaseCommand):
     help = "Walk the resources, builds the db, and generates the static site"
 
     def handle(self, *args, **options):
-        walkers = find_walkers()
+        self.walkers = find_walkers()
 
-        print walkers
+    def walk(self):
+        """Walk to walkers to populate the db"""
+
+        for walker in self.walkers:
+            logging.info('Walking %s', walker)
+            walker()
