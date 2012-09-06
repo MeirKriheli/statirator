fe94fedeb941222ea28e06ffccf0ba0611dea931
Meir Kriheli <mkriheli@gmail.com>
2012-09-06 19:38:25 +0300

    Initial test for parse_rst

--- statirator/core/tests.py
+++ statirator/core/tests.py
@@ -1,6 +1,36 @@
 from django.utils import unittest
 from statirator.core.utils import find_readers
 from statirator.core.readers import dummy_reader
+from statirator.core.parsers import parse_rst
+
+
+TEST_DOC = """
+:slug: some-post-title-slugified
+:draft: 1
+:date: yyyy-mm-dd hh:mm:ss
+
+.. --
+
+=================
+Some post title
+=================
+
+:lang: en
+:tags: Tag1, Tag2
+
+The content of the post
+
+.. --
+
+====================
+The title in Hebrew
+====================
+
+:lang: he
+:tags: Heb Tag1|slug, Heb Tag2|slug
+
+The content of the post in hebrew
+"""
 
 
 class CoreTestCase(unittest.TestCase):
@@ -10,3 +40,14 @@ class CoreTestCase(unittest.TestCase):
 
         readers = find_readers()
         self.assertIn(dummy_reader, readers)
+
+    def test_rst_parser(self):
+        """Correctly parse multilingual rst documents"""
+
+        parsed = parse_rst(TEST_DOC)
+        generic_metadata = parsed[0]
+        self.assertEqual(generic_metadata, {
+            'slug': 'some-post-title-slugified',
+            'draft': '1',
+            'date': 'yyyy-mm-dd hh:mm:ss'
+        })
