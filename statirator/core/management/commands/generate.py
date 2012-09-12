873a7ae5a2d86c12ba98beab99f55c20c04138d2
Meir Kriheli <mkriheli@gmail.com>
2012-09-12 18:52:00 +0300

    Add staticsitegen to generate

--- statirator/core/management/commands/generate.py
+++ statirator/core/management/commands/generate.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import logging
 
 from django.core.management import call_command
@@ -11,10 +12,19 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        print("Syncing in memory db\n" + 20 * '-')
         # make sure we have the db
         call_command('syncdb', load_initial_data=False, interactive=False)
+
+        # load resources
         self.readers = find_readers()
         self.read_resources()
+
+
+        print("\nGenerating static pages\n" + 22 * '-')
+
+        # Use django-medusa for static pages
+        call_command('staticsitegen')
 
     def read_resources(self):
         """Walk to readers to populate the db"""
