52cb80dd92ceabd7d2efe67c0a89f76cd701283b
Meir Kriheli <mkriheli@gmail.com>
2012-09-12 11:50:02 +0300

    Add statirator.core for all commands except test

--- statirator/main.py
+++ statirator/main.py
@@ -3,15 +3,12 @@ import sys
 
 
 def main():
-    # init is a special case, cause we want to add statirator.core to
-    # INSTALLED_APPS, and have the command picked up. we'll handle it in here
-
-    if 'init' in sys.argv:
-        from django.conf import settings
-        settings.configure(INSTALLED_APPS=('statirator.core', ))
-    elif 'test' in sys.argv:
+    if 'test' in sys.argv:
         os.environ.setdefault(
             "DJANGO_SETTINGS_MODULE", "statirator.test_settings")
+    else:
+        from django.conf import settings
+        settings.configure(INSTALLED_APPS=('statirator.core', ))
 
     from django.core import management
     management.execute_from_command_line()
