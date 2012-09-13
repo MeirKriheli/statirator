36013a427767f70138d5cb6f9bf606f02390e0bc
Meir Kriheli <mkriheli@gmail.com>
2012-09-14 01:53:19 +0300

    Added posts archive view

--- statirator/blog/views.py
+++ statirator/blog/views.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from .models import Post
 
 
@@ -8,5 +9,12 @@ class PostView(DetailView):
 
     def get_queryset(self):
         qs = Post.objects.filter(language=self.request.LANGUAGE_CODE)
+        return qs
 
+
+class ArchiveView(ListView):
+
+    def get_queryset(self):
+        qs = Post.objects.filter(language=self.request.LANGUAGE_CODE,
+                                 is_published=True).order_by('-pubdate')
         return qs
--- statirator/project_template/project_name/urls.py
+++ statirator/project_template/project_name/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, url
 from django.conf.urls.i18n import i18n_patterns
-from statirator.blog.views import PostView
+from statirator.blog import views
 
 
 urlpatterns = patterns(
     '',
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<slug>[-\w]+)/$',
-        PostView.as_view(), name='blog_post'),
+        views.PostView.as_view(), name='blog_post'),
+    url('r^archive/$', views.ArchiveView.as_view(), name='blog_archive')
 )
 
 # make all the urls patterns again, with i18n translations, that way default
