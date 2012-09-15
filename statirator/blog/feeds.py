355116b63e375d1384ac7b0cd41324266b8f5228
Meir Kriheli <mkriheli@gmail.com>
2012-09-15 03:24:03 +0300

    Finish per tag rss

--- statirator/blog/feeds.py
+++ statirator/blog/feeds.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import
 from django.conf import settings
-from statirator.core.syndication import LanguageFeed
+from django.shortcuts import get_object_or_404
 
-from .models import Post
+from statirator.core.syndication import LanguageFeed
+from .models import Post, I18NTag
 
 
 class PostsFeed(LanguageFeed):
@@ -34,7 +35,26 @@ class PostsFeed(LanguageFeed):
 
 class TagFeed(LanguageFeed):
 
-    def items(self):
-        pass
+    def get_object(self, request, slug):
+        return get_object_or_404(
+            I18NTag, slug_no_locale=slug,
+            language=request.LANGUAGE_CODE)
 
+    def items(self, obj):
+        return Post.objects.filter(
+            language=obj.language,
+            is_published=True,
+            tags__slug__in=[obj.slug]).order_by('-pubdate')
 
+    def link(self, obj):
+        return obj.get_absolute_url()
+
+    def item_title(self, item):
+        return item.title
+
+    def item_pubdate(self, item):
+        return item.pubdate
+
+    def item_description(self, item):
+        # TODO return item excerpt if exists
+        return item.content
