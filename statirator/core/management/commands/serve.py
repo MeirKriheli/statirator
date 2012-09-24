788eb565a7b51c634f98f68f59a5c1b1c5077bd1
Meir Kriheli <mkriheli@gmail.com>
2012-09-24 11:38:41 +0200

    Auto generate option

--- statirator/core/management/commands/serve.py
+++ statirator/core/management/commands/serve.py
@@ -1,12 +1,18 @@
 from __future__ import print_function, absolute_import
+
 import os
-import SocketServer
+import time
+import threading
+
 import BaseHTTPServer
 import SimpleHTTPServer
-from optparse import make_option
+import SocketServer
+from django.conf import settings
 from django.core.management import call_command
 from django.core.management.base import NoArgsCommand, BaseCommand
-from django.conf import settings
+from optparse import make_option
+
+from statirator.core.utils import filesystem_changed
 
 
 class ThreadedHTTPServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
@@ -20,31 +26,58 @@ class Command(NoArgsCommand):
     option_list = (
         make_option('--port', '-p', dest='port', default=8000, type='int',
                     help='The port to listen [Default: %default]'),
-        make_option('--auto-rebuild', '-r', dest='rebuild',
-                    action='store_true', help='Auto rebuild static site on '
+        make_option('--auto-generate', '-g', dest='generate',
+                    action='store_true', help='Auto generate static site on '
                     'each modification'),
     ) + BaseCommand.option_list
 
     def handle_noargs(self, **options):
 
-        # Genarate on first run
-        self.generate()
-
+        self.httpd_thread = None
+        self.httpd_server = None
+        self._shutdown = False
         curr_dir = os.getcwd()
+
         try:
+            if options['generate']:
+                self.filesystem_watcher()
+            else:
+                call_command('generate')
+
             os.chdir(settings.BUILD_DIR)
-            self.serve(options['port'], options['rebuild'])
+            self.serve(options['port'])
+
+            while not self._shutdown:
+                try:
+                    time.sleep(1)
+                except KeyboardInterrupt:
+                    self.shutdown()
         finally:
             os.chdir(curr_dir)
 
-    def generate(self):
-        call_command('generate')
+    def shutdown(self):
+        print("\nShutting down")
+        self._shutdown = True
+        self.httpd_server.shutdown()
+        self.httpd_thread.join()
 
-    def serve(self, port, rebuild):
-        server = ThreadedHTTPServer(('', port), SimpleHTTPServer.SimpleHTTPRequestHandler)
+    def filesystem_watcher(self):
+        lock = threading.Lock()
 
-        try:
-            # TODO Handle auto rebuild
-            server.serve_forever()
-        except KeyboardInterrupt:
-            print('Shutting down')
+        def watch():
+            while not self._shutdown:
+                with lock:
+                    if filesystem_changed(settings.ROOT_DIR, ignore_dirs=[settings.BUILD_DIR]):
+                        call_command('generate')
+                    time.sleep(1)
+
+        watcher_thread = threading.Thread(target=watch)
+        watcher_thread.start()
+
+    def serve(self, port):
+        self.httpd_server = ThreadedHTTPServer(
+            ('', port), SimpleHTTPServer.SimpleHTTPRequestHandler)
+
+        self.httpd_thread = threading.Thread(target=self.httpd_server.serve_forever)
+        self.httpd_thread.setDaemon(True)
+        self.httpd_thread.start()
