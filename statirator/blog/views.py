4a08a91c60e8f2acbd1a66fe0c9bc75d092e06db
Meir Kriheli <mkriheli@gmail.com>
2012-10-05 03:24:56 +0200

    dummy translation object in views

--- statirator/blog/views.py
+++ statirator/blog/views.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.base import TemplateView
+from statirator.core.models import DummyTranslation
 
 from .models import Post, I18NTag
 
@@ -24,6 +25,13 @@ class ArchiveView(ListView):
         qs = Post.objects.filter(language=self.request.LANGUAGE_CODE,
                                  is_published=True).order_by('-pubdate')
         return qs
+
+    def get_context_data(self, **kwargs):
+        # Add dummy object for translations in menu to display translation to
+        # this view
+        ctx = super(ArchiveView, self).get_context_data(**kwargs)
+        ctx['object'] = DummyTranslation(self.request)
+        return ctx
 
 
 class TagView(DetailView):
@@ -53,3 +61,10 @@ class TagView(DetailView):
 class TagsView(TemplateView):
 
     template_name = 'blog/tag_list.html'
+
+    def get_context_data(self, **kwargs):
+        # Add dummy object for translations in menu to display translation to
+        # this view
+        ctx = super(TagsView, self).get_context_data(**kwargs)
+        ctx['object'] = DummyTranslation(self.request)
+        return ctx
